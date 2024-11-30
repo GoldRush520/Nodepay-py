d4963a14f0d081c1d49cf40945b3adba9750c6f3
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-30 11:07:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 PROXY_FILE_PATH = "proxy.txt"
 TOKEN_FILE_PATH = "tokens.txt"
 
@@ -6,6 +5,16 @@ MAX_RETRIES = 5
 MAX_CONNECTIONS_PER_ACCOUNT = 3
 
 API_ENDPOINTS = {
-    "SESSION": "https://api.nodepay.ai/api/auth/session",
-    "PING": "http://52.77.10.116/api/network/ping"
+    "SESSION": "https://api.nodepay.ai/api/auth/session",  
+    "PING": [ 
+        "http://52.77.10.116/api/network/ping",
+        "http://13.215.134.222/api/network/ping",
+        "http://18.136.143.169/api/network/ping",
+        "http://52.74.35.173/api/network/ping",
+        "http://18.142.214.13/api/network/ping",
+        "http://18.142.29.174/api/network/ping",
+        "http://52.74.31.107/api/network/ping",
+        "http://3.1.154.253/api/network/ping"
+    ]
 }
+
