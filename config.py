994176a5dd8ee614fe2baa9c5303d3a592653054
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-30 09:45:19 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+
+PROXY_FILE_PATH = "proxy.txt"
+TOKEN_FILE_PATH = "tokens.txt"
+
+MAX_RETRIES = 5
+MAX_CONNECTIONS_PER_ACCOUNT = 3
+
+API_ENDPOINTS = {
+    "SESSION": "https://api.nodepay.ai/api/auth/session",
+    "PING": "http://52.77.10.116/api/network/ping"
+}
